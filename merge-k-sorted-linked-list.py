97eb21baca5c00c9d4d31dce08d2de07ef23d5fa
Bhavya Shah <bhavyashah9962@gmail.com>
2020-10-28 19:04:05 +0530

    You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
    Merge all the linked-lists into one sorted linked-list and return it.

---
+++ merge-k-sorted-linked-list.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        merged = None
+        other = None
+        if l1.val <= l2.val:
+            merged = l1
+            other = l2
+        else:
+            merged = l2
+            other = l1
+        curr = merged
+        while curr != None:
+            if other == None:
+                break
+            if curr.next == None:
+                curr.next = other
+                break
+            if(curr.val <= other.val and other.val <= curr.next.val):
+                temp = curr.next
+                curr.next = other
+                temp2 = other.next
+                other.next = temp
+                other = temp2
+                curr = curr.next
+            else:
+                curr = curr.next
+        return merged
+    
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if len(lists) == 0:
+            return
+        i = 0
+        while True:
+            try:
+                first = lists[i]
+                second = lists[i+1]
+                firstTwoMerged = self.mergeTwoLists(first, second)
+                lists = [firstTwoMerged] + lists[2:]
+            except:
+                break
+        return lists[0]
