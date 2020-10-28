ea3846570d0a2fcd3e46b439a679051a21dec561
Bhavya Shah <bhavyashah9962@gmail.com>
2020-10-28 13:57:35 +0530

    Write a program to find the node at which the intersection of two singly linked lists begins.

---
+++ linked-list-intersection.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        a = {ListNode(None)}
+        curr = headA
+        while curr:
+            a.add(curr)
+            curr = curr.next
+        curr2 = headB
+        while curr2:
+            if curr2 in a:
+                return curr2
+            curr2 = curr2.next
+        return None
